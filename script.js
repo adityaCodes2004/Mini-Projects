4b510a17c8f7affb11b3e088638ba7ed5b5ea980
adityaCodes2004 <shrivastavaaditya242@gmail.com>
2024-06-10 10:18:36 +0530

    Guessing game

---
+++ script.js
@@ -0,0 +1,41 @@
+let input = document.getElementById('input');
+let btn = document.getElementById('btn');
+let wrng = document.querySelector('.wrng');
+let guesses = document.getElementById('guesses');
+
+
+let answer = Math.floor(Math.random()*100) + 1;
+let numGuesses = 0;
+
+btn.addEventListener('click',() => {
+    guessNumber();
+})
+
+function guessNumber() {
+    if(input.value<1 || input.value>100 || isNaN(input.value)) {
+        wrng.innerHTML = "Enter a number between 1 to 100";
+    } else {
+        numGuesses ++;
+        guesses.innerHTML = "No.of Guess : "+numGuesses;
+
+        if(input.value > answer) {
+            wrng.innerHTML = "You guessed too high";
+            input.value = "";
+        } else if(input.value < answer) {
+            wrng.innerHTML = "You guessed too low";
+            input.value="";
+        } else {
+            wrng.innerHTML="Congratulations! You guessed the correct answer";
+            setTimeout(() =>{
+                resetGame();
+            },5000)
+        }
+    }
+}
+
+function resetGame() {
+    numGuesses = 0;
+    answer = Math.floor(Math.random()*100) + 1;
+    input.value = "";
+    guesses.innerHTML = 'No of Guess:0';
+}
